3060731b9469822d038d6688aed69749742c0c91
Rafi Khaled <rkhaled@vassar.edu>
2018-06-26 16:14:13 -0700

    some testing with smart farm dataset

---
+++ Syngenta-RFEVC.py
@@ -0,0 +1,10 @@
+# THERE ARE SEPARATE NOTEBOOKS FOR VISUALIZATIONS, DATASET ANALYSIS, ETC. IN THE REPO.
+
+import pandas as pd
+import numpy as np
+
+# READ THE CSV INTO DATAFRAME
+
+df = pd.read_csv('Syngenta/Syngenta_2017/Experiment_dataset.csv')
+
+# df_sf = pd.read_csv('Smart_Farm_dataset.csv')
